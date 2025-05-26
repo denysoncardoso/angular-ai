fbf19daebd599f4afee4caada9317821efb1df8f
DESKTOP-8BNEGM8\Denyson Cardoso <denyson.cardoso@hotmail.com>
2025-05-26 20:41:08 -0300

    Implementa formulário reativo para envio de mensagens no chat simples, substituindo a estrutura anterior. Adiciona validação e reset do formulário após o envio.

--- src/app/chat/simple-chat/simple-chat.ts
+++ src/app/chat/simple-chat/simple-chat.ts
@@ -4,10 +4,12 @@ import { MatToolbar } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-simple-chat',
   imports: [
+    ReactiveFormsModule,
     MatCardModule,
     MatToolbar,
     MatInputModule,
@@ -18,11 +20,22 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './simple-chat.scss'
 })
 export class SimpleChat {
-  constructor() { }
-
+  chatForm: FormGroup;
   title = 'Simple Chat';
   messages = signal([
     { text: 'Como posso ajudar?', isBost: true }
   ]);
 
+  constructor(private fb: FormBuilder) {
+    this.chatForm = this.fb.group({
+      messageText: ['']
+    });
+  }
+
+  sendMessage() {
+    if (this.chatForm.valid && this.chatForm.value.messageText?.trim()) {
+      this.messages.update(messages => [...messages, { text: this.chatForm.value.messageText, isBost: false }]);
+      this.chatForm.reset();
+    }
+  }
 }
